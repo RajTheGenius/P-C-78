c8b49a9fb3750e0cd688b92f9036edf48421aa16
RajTheGenius <89466456+RajTheGenius@users.noreply.github.com>
2022-10-29 20:43:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ function update()
     var numbers_of_family_member_in_array = 5
     if(i > numbers_of_family_member_in_array)
       {
-          i = 0;
+        i = 0;
       }
     
     //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [i];
+    var updatedImage = images[i];
     //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [i] ;
+    var updatedName = names[i] ;
  
     document.getElementById("family_member_image").src = updatedImage;
     document.getElementById("family_member_name").innerHTML = updatedName;
